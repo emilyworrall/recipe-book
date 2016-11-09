94a617e68626835f3f0eed0dbde096a91e4ae717
Emily Worrall <emily@festicket.com>
2016-11-09 11:06:14 +0000

    Setup recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RepicesComponent } from './repices/repices.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RepicesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/repices/repices.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-repices',
+  templateUrl: './repices.component.html'
+})
+export class RepicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
